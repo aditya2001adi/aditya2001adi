23aebe655d5d555d872a2d127297eb21a102222e
aditya <aditya2001adi@gmail.com>
2022-04-16 15:47:57 -0700

    commit on 'main'

---
+++ axb.py
@@ -0,0 +1,33 @@
+import numpy as np 
+print("Enter each row of your first matrix. After you've entered the last row, enter an empty row.")
+A = []
+while True:
+    a = input("Row:") #input Rows
+    if not a.strip():
+        break #break after user inputs empty row
+    A.append(list(map(int, a.split()))) #append rows to list of lists
+
+
+print("A = ", A)
+
+for x in A:
+    col_number_A = (len(x)) 
+
+
+print("Enter each row of your second matrix. After you've entered the last row, enter an empty row.")
+B = []
+while True:
+    b = input("Row:") #input rows of second matrix
+    if not b.strip():
+        break #break after user inputs empty row
+    B.append(list(map(int, b.split()))) #append rows to list of lists
+
+print("B = ", B)
+
+row_number_B = len(B) 
+
+if col_number_A == row_number_B: #ensure number of columns of matrix A and number of rows of matrix B are the same
+    result = np.dot(A, B)
+    print(result)
+else:
+    print("These matrices cannot be multiplied. Make sure the number of columns in Matrix A corresponds to the number of rows in Matrix B!")
